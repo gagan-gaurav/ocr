dae619d3d949f2a46b361e6d49adfb90d70b2f4b
gagan-gaurav <iamgagangaurav@gmail.com>
2021-12-18 17:37:17 +0530

    com

--- cnn.py
+++ cnn.py
@@ -4,5 +4,14 @@ import os
  
 path = 'myData'
 
+images = []
 myList = os.listdir(path)
 print(myList)
+noOfClasses = len(myList)
+
+for x in range(0, noOfClasses):
+	myPicList = os.listdir(path + '/' + str(x))
+	for y in myPicList:
+		currentImg = cv.imread(path + '/' + str(x) + str(y))  
+		currentImg = cv.resize(currentImg, (32, 32))
+		images.append(currentImg)
